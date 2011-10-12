432d39fc504c6e44bca56bf7221e79ac750cacfa
Mikey <pmsorhaindo@gmail.com>
2011-10-12 14:33:56 +0100

    Got GUI! ;)

---
+++ src/GUI.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+public class GUI implements Runnable{
+	public GUI(){
+		
+	JFrame window = new JFrame("Restaurant Simulation");
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.setVisible(true);
+	window.setSize(500,500);
+	window.setLocation(100,100);
+	}
+	
+	public void run(){
+		
+		
+		
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
 	
 	public static void main(String[] args){
 		OrderList kitchenYo = new OrderList();
+		Thread window = new Thread(new GUI(),"window");
 		Thread server1 = new Thread(new Server(kitchenYo), "server1");	
 		Thread cashier1 = new Thread(new Cashier("Bob",kitchenYo), "cashier1");
 		Thread cook1 = new Thread(new Cook("Fred",kitchenYo), "cook1");
 		
+		window.start();
 		server1.start();
 		cashier1.start();
 		cook1.start();		
